52f2a77dafa319a65bae759bf1f65d24a3ecd1b2
hyennii <alqkf@icloud.com>
2024-05-11 19:47:28 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Potato from './potato';   //하나의 컴포넌트만 렌더링해야하기 때문에, App 안에 Potato 넣기
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello :)</h1>
+      <Potato/>    
     </div>
   );
 }
